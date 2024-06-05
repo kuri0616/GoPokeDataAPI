b4cb6aa0b1c519caf09a4ae546900d291cdc02ab
日下陸矢 <rikuya.98@gmail.com>
2024-06-05 21:11:07 +0900

    ポケモンデータを取得するハンドラーを定義、IDに紐づく種族値をレスポンスとして返すようにする

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,52 @@
 package main
 
 import (
-	"io"
+	"encoding/json"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
 func main() {
-	testHandler := func(w http.ResponseWriter, r *http.Request) {
-		_, err := io.WriteString(w, "Hello, World!")
+	type Stat struct {
+		Name string `json:"name"`
+	}
+	type Stats struct {
+		BaseStat int  `json:"base_stat"`
+		Stat     Stat `json:"stat"`
+	}
+	type PokeData struct {
+		Stats []Stats `json:"stats"`
+	}
+
+	//ポケモンのデータを取得するhandler
+	GetPokeDataHandler := func(w http.ResponseWriter, req *http.Request) {
+		vars := mux.Vars(req)
+		//パスパラメータを数値に変換
+		var PokeData PokeData
+		res, err := http.Get("https://pokeapi.co/api/v2/pokemon/" + vars["id"])
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer res.Body.Close()
+
+		//レスポンスを構造体に変換
+		if err := json.NewDecoder(res.Body).Decode(&PokeData); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		//レスポンスをjson形式で返す
+		w.Header().Set("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(PokeData); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 	}
 
-	http.HandleFunc("/test", testHandler)
+	r := mux.NewRouter()
+	r.HandleFunc("/test/{id:[0-9]+}", GetPokeDataHandler).Methods(http.MethodGet)
+	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
